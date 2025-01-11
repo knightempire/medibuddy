efb1dd76d5f118199982da883d4981fbf5723fda
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-01-11 10:02:53 +0530

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,27 +17,23 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <head>
-        <title>ExploreIQ</title>
+        <title>MediBuddy</title>
         
         {/* Meta tags for SEO */}
         <meta
-          name="Travel based Recommendation System"
-          content="A Travel Recommendation System & Trip Planning Platform. Welcome to ExploreIQ! This platform helps users plan their trips by recommending destinations, activities, and experiences based on personalized preferences."
+          name="MediBuddy"
+          content="Book Health Checks, Lab tests, Online Medicine & Doctor Consultation | MediBuddy."
         />
-        <meta name="author" content="ExploreIQ Team - https://github.com/knightempire/travel" />
+        <meta name="author" content=" Team - https://github.com/knightempire/" />
 
-     <meta name="keywords" content="travel recommendation, trip planning, personalized travel, vacation planning, destinations, activities, experiences, ExploreIQ, Explore , Explore IQ " />
-
-<meta name="description" content="ExploreIQ is a personalized travel recommendation system to help you plan your trips with curated destinations, activities, and experiences tailored to your preferences." />
-<meta name="application-name" content="ExploreIQ" />
+     <meta name="keywords" content="MediBuddy, Healthcare, Cashless Healthcare, Health Check Packages, Book health checks, book online lab tests, book blood tests, book online consultation, online doctor consultation, online dental consultation, dental packages, online teleconsultation, order online medicines, cashless hospitalisation, Medi Assist network hospitals." />
 
 
-<meta property="og:title" content="ExploreIQ - Personalized Travel Recommendations" />
-<meta property="og:description" content="Plan your next trip with ExploreIQ, a travel platform offering personalized recommendations for destinations and activities based on your unique preferences." />
-<meta property="og:image" content="https://www.exploreiq.com/images/og-image.jpg" />
-<meta property="og:url" content="https://www.exploreiq.com/" />
+
+<meta property="og:title" content="MediBuddy " />
+<meta property="og:description" content="Book Health Checks, Lab tests, Online Medicine & Doctor Consultation | MediBuddy." />
 <meta property="og:type" content="website" />
-<meta property="og:site_name" content="ExploreIQ" />
+
 
 <link rel="icon" href="https://www.exploreiq.com/favicon.ico" />
 
